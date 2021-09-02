2ab9ea7a32771e7abc8a5226775fa8c07b14d3fe
pujatB <pujatbapat@gmail.com>
2021-09-02 12:00:48 +0100

    changes made to headings and adds a paragraph

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
+// 2.1. Changes heading background colour
 document.querySelector("h1").addEventListener("click",()=>{
-    
+    document.querySelector("h1").style.backgroundColor = "cyan"
+})
+
+
+//  Changes heading background colour back to normal
+document.querySelector("h1").addEventListener("mouseout",()=>{
+    document.querySelector("h1").style.backgroundColor = "white"
+})
+
+// 2.3 If someone types a letter whilst viewing the webpage a new paragraph should be created 
+// and that letter should be added to it.
+document.addEventListener("keydown",e=>{
+    let newElement = document.createElement("p");
+    let section = document.querySelector("section")
+    newElement.textContent = e.key;
+    section.append(newElement);
+
 })
