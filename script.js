6abada970370b4abf039536878a899dc9e63de4a
velinkalenderski <velinkalenderski@hotmail.co.uk>
2021-09-02 12:04:03 +0100

    change h2 on hover

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
-document.querySelector("h1").addEventListener("click",()=>{
-    
+// If a user hovers over a h2 tag replace the words inside it to contain a greeting
+
+let heading2 = document.querySelector('.heading2');
+console.log(heading2);
+heading2.addEventListener('mouseover', () => {
+    heading2.textContent = 'Welcome To Our Website';
 })
+
+heading2.addEventListener('mouseout', () => {
+    heading2.textContent = 'Free Class';
+})
+
+
+
